cf75d3428a323355b3d250e23e9660f082fea475
H. İbrahim Güngör <igungor@gmail.com>
2015-11-11 00:11:56 +0200

    Fix build on GCC5.1
    Building host-localedef fails reporting multiple definitions and undefined references.

--- package/localedef/localedef.mk
+++ package/localedef/localedef.mk
@@ -12,6 +12,10 @@ HOST_LOCALEDEF_CONF_OPTS += \
 	--prefix=/usr \
 	--with-glibc=./eglibc
 
+# Fix build on GCC5.1. Building host-localdef fails reporting multiple
+# definitions and undefined references
+HOST_LOCALEDEF_CONF_ENV = CFLAGS="$(HOST_CFLAGS) -fgnu89-inline"
+
 # The makefile does not implement an install target
 define HOST_LOCALEDEF_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/localedef $(HOST_DIR)/usr/bin/localedef
